6d958bce00a9fbc201df9d31b0ea17bb403d3350
jaspercurry <jaspercurry@gmail.com>
2016-02-22 21:11:45 -0500

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(number)
   if number % 15 == 0
     return "FizzBuzz"
-end
-  if number % 5 == 0
+  elsif number % 5 == 0
     return "Buzz"
-end
-  if number % 3 == 0
+  elsif number % 3 == 0
    return "Fizz"
+  else 
+    return nil
 end 
 end
